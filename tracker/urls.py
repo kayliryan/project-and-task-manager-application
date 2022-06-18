e628c0335a8d03154a8eed37baa91a74388b80ee
Kayli <kayliryan@gmail.com>
2022-06-17 17:09:58 -0700

    Feature 6 complete

--- tracker/urls.py
+++ tracker/urls.py
@@ -13,10 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.views.generic.base import RedirectView
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
 
 urlpatterns = [
+    path(
+        "",
+        RedirectView.as_view(url=reverse_lazy("list_projects")),
+        name="home",
+    ),
     path("admin/", admin.site.urls),
     path("projects/", include("projects.urls")),
 ]
